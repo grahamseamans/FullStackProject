27a4fc44d96145a533214895bdc4f1a5c7d627b4
Graham Seamans <seamansgraham@gmail.com>
2021-05-26 16:41:42 -0700

    main page cards getting better

--- jet-set-front-end/src/components/CityCard.js
+++ jet-set-front-end/src/components/CityCard.js
@@ -1,13 +1,55 @@
 import { CityGraphics } from "./CityGraphics";
 
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles({
+  root: {
+    display: "flex",
+    minWidth: 500,
+    height: 300,
+    padding: 10,
+    margin: 20,
+  },
+  details: {
+    display: "flex",
+    flexDirection: "column",
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+    graphics: {
+      width: "auto",
+  }
+});
+
 export function CityCard(props) {
+  const classes = useStyles();
+
   const { name, temperature, wind } = props;
   return (
-    <span class='city-card'>
-      {name}, (<CityGraphics temperature={temperature} wind={wind}></CityGraphics>),{" "}
-      {temperature}
-    </span>
+    <Card className={classes.root}>
+      <div className={classes.details}>
+        <CardActions>
+          <Button size="large"> {name}</Button>
+        </CardActions>
+        <Typography variant="h5" component="h2">
+          Temp: {temperature}
+        </Typography>
+        <Typography className={classes.pos} color="textSecondary">
+          Wind: {wind}
+        </Typography>
+      </div>
+      <div className={classes.graphics}>
+        <CityGraphics temperature={temperature} wind={wind}></CityGraphics>
+      </div>
+    </Card>
   );
 }
-
-// export default CityCard;
--- jet-set-front-end/src/components/CityWrapper.js
+++ jet-set-front-end/src/components/CityWrapper.js
@@ -4,6 +4,9 @@ export function CityWrapper(props) {
     const {cityList} = props
     return (
         <div class='main-flexbox'>
+            {/* {cityList.map((cityArgs) =>
+                (<CityCard props={cityArgs}></CityCard>)
+            )} */}
             {cityList.map(({ name, temperature, wind}) =>
                 (<CityCard name={name} temperature={temperature} wind={wind}></CityCard>)
             )}
