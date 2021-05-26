16bb345b02743ad05756662b3241980bd80cfbfa
Graham Seamans <seamansgraham@gmail.com>
2021-05-26 15:31:25 -0700

    Added material ui to packages

--- jet-set-front-end/src/components/CityGraphics.js
+++ jet-set-front-end/src/components/CityGraphics.js
@@ -25,11 +25,11 @@ puts it to a color
 */
 
 function Box(props) {
-  const { position, temperature, wind } = props;
+  const { temperature, wind } = props;
   // This reference will give us direct access to the THREE.Mesh object
   const mesh = useRef();
   // Set up state for the hovered and active state
-  const [hovered, setHover] = useState(false);
+//   const [hovered, setHover] = useState(false);
   const [active, setActive] = useState(false);
   // Subscribe this component to the render-loop, rotate the mesh every frame
     // useFrame((state, delta) => (mesh.current.rotation.x += windToSpin(wind)));
@@ -45,8 +45,8 @@ function Box(props) {
       ref={mesh}
       scale={active ? 1.5 : 1}
       onClick={(event) => setActive(!active)}
-      onPointerOver={(event) => setHover(true)}
-      onPointerOut={(event) => setHover(false)}
+    //   onPointerOver={(event) => setHover(true)}
+    //   onPointerOut={(event) => setHover(false)}
     >
       <boxGeometry args={[1, 1, 1]} />
       {/* <meshStandardMaterial color={hovered ? "hotpink" : "orange"} /> */}
