14025c3d5b4c59a5e28f8012ecfc46dad7f7320d
DC <chased@pdx.edu>
2021-05-26 05:52:11 -0700

    added query for ticketmaster

--- node_backend/server.js
+++ node_backend/server.js
@@ -6,6 +6,7 @@ const app = express()
 const port = 3000
 
 weather_api = process.env.OPEN_WEATHER_API_KEY;
+ticketmaster_api = process.env.TICKETMASTER_API_KEY;
 app.use(express.json())
 
 app.get('/', (req, res) => {
@@ -14,12 +15,33 @@ app.get('/', (req, res) => {
 
 app.get('/weather', (req, res) => {
 
-    let city = req.body.city;
+    let city = req.query.city
+    let country = req.query.country
 
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${weather_api}`)
     .then(res => res.json())
     .then(json => {
         res.send(json)
+    })
+})
+
+app.get('/events', (req, res) => {
+
+    let city = req.query.city
+    let country = req.query.country
+    console.log(city);
+
+    fetch(`https://app.ticketmaster.com/discovery/v2/events?apikey=${ticketmaster_api}&radius=50&unit=miles&locale=*&city=${city}`)
+    .then(res => res.json())
+    .then(json => {
+        names = [];
+        
+        json._embedded.events.forEach(event => {
+            //console.log(event);
+            names.push(`${event.dates.start.localDate}:  ${event.name}`)
+            console.log(event.name + event.dates.start.localDate)})
+        //console.log(json._embedded.events);
+        res.send(names);
     })
 })
 
