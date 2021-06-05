16bc6a008786b87aec12a6c4f7295af2d7345985
Graham Seamans <seamansgraham@gmail.com>
2021-06-05 12:55:50 -0700

    changed static serve to relative link

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ autocomplete_api = process.env.AUTOCOMPLETE_API_KEY;
 
 app.use(express.json());
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'jet-set-front-end/build')));
+// app.use(express.static(path.join(__dirname, 'jet-set-front-end/build')));
+app.use(express.static('./jet-set-front-end/build'));
 
 function checkResponseStatus(res) {
   console.log("checkingResponseStatus for", res)
